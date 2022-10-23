e0c848f30f36a052067958c81d07faab328be004
WatDuhHekBro <watduhhekbro@protonmail.com>
2022-10-23 17:09:44 -0500

    Did some quick and dirty refactoring

--- downloader.py
+++ downloader.py
@@ -1,78 +1,67 @@
 import os
+import re
+
+# Match /watch?v= URLs and allow using links from alternate YouTube frontends directly
+ytvideo_pattern = re.compile(r'https:\/\/.+?(\/watch\?v=.{11})')
+# Do the same for YouTube playlists
+playlist_pattern = re.compile(r'https:\/\/.+?(\/playlist\?list=.+)')
 
 flags = {
 	'video': '-f bestvideo+bestaudio/best',
+	'all-fragments': '--abort-on-unavailable-fragment',
+	'novideo': '--skip-download',
+	'keep': '-k',
 	'audio': '-x --audio-format mp3',
 	'hq': '--audio-quality 0',
 	'subs': '--all-subs',
 	'desc': '--write-description',
-	'thumb': '--write-all-thumbnails --embed-thumbnail'
-}
-presets = {
-	# youtube-dl -f bestvideo+bestaudio/best 
-	'video': flags['video'],
-	# youtube-dl -f bestvideo+bestaudio/best --all-subs --write-description --write-all-thumbnails --embed-thumbnail 
-	'videoext': '{} {} {} {}'.format(flags['video'], flags['subs'], flags['desc'], flags['thumb']),
-	# youtube-dl -x --audio-format mp3 
-	'audio': flags['audio'],
-	# youtube-dl -x --audio-format mp3 --audio-quality 0 
-	'audiohq': '{} {}'.format(flags['audio'], flags['hq'])
+	'thumb': '--write-all-thumbnails',
+	'thumb-embed': '--embed-thumbnail'
 }
 
-url = input("Input URLs (multiple URLs separated by spaces): ")
+url = input("Input URLs (space-separated): ")
 
 if not url:
-	print("WARNING: URLs are empty! Exiting program.")
+	print("ERROR: URLs are empty! Exiting program.")
 else:
 	url = url.split(' ')
 	print("Counting " + str(len(url)) + " URL(s).")
-	go = False
-	both = False
-	
-	args = input("\nEnter one of the following presets or input flags:\n- video\n- videoext\n- audio\n- audiohq\n- flags\n- both\nPreset: ")
-	
-	if args == 'flags':
-		f = input("\nNext, enter any flags:\n- video\n- audio\n- hq\n- subs\n- desc\n- thumb\nSpace Separated Flags: ").split(' ')
-		args = ''
-		
-		for x in f:
-			if x in flags:
-				args += ' ' + flags[x]
-		
-		go = True
-	elif args == 'both':
-		args = ''
-		video = presets['videoext'] if input("\nDo you want extended video data? (Leave blank for standard video data) [ext/st] ") == 'ext' else presets['video']
-		audio = presets['audio'] if input("Do you want HQ audio or standard audio? (Leave blank for HQ audio) [hq/st] ") == 'st' else presets['audiohq']
-		go = True
-		both = True
-	elif args in presets:
-		args = presets[args]
-		go = True
-	else:
-		print("\nThe preset you entered is not valid. Exiting program.")
-	
-	if go:
-		if input("\nAre these playlists URLs? (Leave blank for \"no\") [y/n] ").lower() == 'y':
-			args += ' -o "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"'
+	args = []
+
+	# List flags
+	print("\nAvailable Flags:")
+
+	for key in flags.keys():
+		print("- {}".format(key))
+
+	# Get flags
+	f = input("\nFlags (space-separated): ").split(' ')
+
+	for x in f:
+		if x in flags:
+			args.append(flags[x])
+
+	# Ask for output location
+	d = input("Custom directory? (Default: \"general\") ")
+	d = d if d else 'general'
+	output = '-o "{}/%(title)s.%(ext)s"'.format(d)
+	print()
+
+	for u in url:
+		# Detect if it's a YouTube video URL (but not necessarily on YouTube)
+		match = re.fullmatch(ytvideo_pattern, u)
+
+		if(match):
+			u = 'https://www.youtube.com' + match.groups()[0]
+
+		match = re.fullmatch(playlist_pattern, u)
+
+		# Detect if it's a playlist URL and handle accordingly, ignore existing output location
+		if(match):
+			u = 'https://www.youtube.com' + match.groups()[0]
+			os.system('youtube-dl {} {} {}'.format(' '.join(args), '-o "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"', u))
 		else:
-			d = input("Custom directory? (Leave blank for \"General\") ")
-			d = d if d else 'General'
-			args += ' -o "{}/%(title)s.%(ext)s"'.format(d)
+			os.system('youtube-dl {} {} {}'.format(' '.join(args), output, u))
 		print()
-		
-		args = args.lstrip()
-		
-		if both:
-			for u in url:
-				os.system('youtube-dl {} {} {}'.format(audio, args, u))
-				print()
-				os.system('youtube-dl {} {} {}'.format(video, args, u))
-				print()
-		else:
-			for u in url:
-				os.system('youtube-dl {} {}'.format(args, u))
-				print()
-	# EOF
-# Add error msg without using cmd.exe?
+
 input("\nPress any key to continue...")
